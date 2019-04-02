9e9ff0dcc80b3a034f634d4549078fce8b7556a4
apoca <vieira@esolidar.com>
2019-04-02 15:19:59 +0100

    closed: added tests for the status checkout

---
+++ tests/Feature/SibsServiceTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Apoca\Sibs\Tests\Feature;
+
+use Apoca\Sibs\Tests\TestCase;
+use Apoca\Sibs\Facade\Sibs;
+
+/**
+ * Class CheckoutTest
+ *
+ * @package Apoca\Sibs\Tests\Feature
+ */
+class SibsServiceTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    /**
+     * @test
+     */
+    public function verifyResponseStatusInvalidEntity(): void
+    {
+        $request = [
+            'brand' => 'CHECKOUT',
+            'amount' => 100,
+            'currency' => 'EUR',
+            'type' => 'DB'
+        ];
+
+        $response = Sibs::checkout($request)->pay();
+        $response2 = Sibs::status($response->response->id);
+
+        $this->assertSame($response->status, 200);
+        $this->assertSame($response2->status, 400);
+    }
+}
